9ef272ae8ebb42d4ca77195a6cb7d5f72a4617f4
QuiKz <104368491+QuiKzyy@users.noreply.github.com>
2022-07-01 12:25:12 +0300

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 9565884687 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/QuiKzyy/Scripts/main/Power%20Clicker%20Simular.lua"))()
+    -- Clicker Simular
+elseif game.PlaceId == 8540346411 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/QuiKzyy/Scripts/main/Rebirth%20Champions.lua"))()
+    -- Clicker Champions X 
+end 
